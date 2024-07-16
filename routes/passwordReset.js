2aeddf768742de13b9453c945de7335927e18f28
BenKasapo <benedykasapo@gmail.com>
2024-07-16 10:07:55 +0100

    routes/PasswordReset corrected again

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -2,6 +2,7 @@ const express = require("express")
 
 const { resetPassword } = require("../controllers/passwordReset")
 
+
 const router = express.Router()
 
 router.post("/reset-password", resetPassword)
